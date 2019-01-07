5c3eeea2867b22159004c0a863eb59ac12f8c581
ewal2 <38157680+ewal2@users.noreply.github.com>
2019-01-07 17:57:14 +0100

    Create emissions-alert.js

---
+++ emissions-alert.js
@@ -0,0 +1,54 @@
+Beginning:
+
+import React from 'react';
+import { Translate } from 'react-i18nify';
+import { Alert } from 'reactstrap';
+
+export const EmissionAlertBadge = () => (
+    <Alert color="warning">
+        <Translate value="emissions.yourEmissionsAreTooHigh" />
+    </Alert>
+);
+
+
+----------------------------------------
+
+First version: 
+
+import React from 'react';
+import { Translate } from 'react-i18nify';
+import { Alert } from 'reactstrap';
+
+function DangerEmissions(props) {
+   return 
+    <Alert color="danger">
+       <Translate value="emissions.yourEmissionsAreTooHigh" />
+    </Alert>  
+}
+
+function StandardEmissions(props) {
+   return 
+   <Alert color="warning">
+        <Translate value="emissions.yourEmissionsAreAverage" />
+    </Alert>  
+}
+
+function LowEmissions(props) {
+   return 
+   <Alert color="success">
+        <Translate value="emissions.yourEmissionsAreLow" />
+    </Alert> 
+}
+
+function EmissionsLevel(props){
+    export const EmissionAlertBadge = props.EmissionAlertBadge;
+    if(EmissionAlertBadge > 20){
+        return < DangerEmissions />
+    }
+    if(EmissionAlertBadge => 10 ){
+        return < StandardEmissions />
+    } else{
+        return < LowEmissions />
+    }
+}
+);
